6a36dd860d86077dac591ea50d0ab02d179af9e9
Phonekham <phone.madridista@gmail.com>
2019-08-25 15:30:38 +0700

    Increase Method Logic

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -154,7 +154,22 @@ class ProductProvider extends Component {
   };
 
   increment = id => {
-    console.log(id);
+    let tempCart = [...this.state.cart];
+    const cartItem = tempCart.find(item => item.id === id);
+    cartItem.count++;
+    cartItem.total = cartItem.count * cartItem.price;
+    cartItem.total = parseFloat(cartItem.total.toFixed(2));
+    this.setState(
+      () => {
+        return {
+          cart: [...tempCart]
+        };
+      },
+      () => {
+        this.addTotals();
+        this.syncStorage();
+      }
+    );
   };
   decrement = id => {
     console.log(id);

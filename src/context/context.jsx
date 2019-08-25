ce1e97126d645f5222978878f560e3bed56841b9
Phonekham <phone.madridista@gmail.com>
2019-08-25 10:15:01 +0700

    SyncStorage Method

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -53,7 +53,13 @@ class ProductProvider extends Component {
 
   // get cart from local storage
   getStorageCart = () => {
-    return [];
+    let cart;
+    if (localStorage.getItem("cart")) {
+      cart = JSON.parse(localStorage.getItem("cart"));
+    } else {
+      cart = [];
+    }
+    return cart;
   };
   // get product from local storage
   getStorageProduct = () => {
@@ -90,7 +96,9 @@ class ProductProvider extends Component {
     });
   };
   // sync storage
-  syncStorage = () => {};
+  syncStorage = () => {
+    localStorage.setItem("cart", JSON.stringify(this.state.cart));
+  };
   //add to cart
   addToCart = id => {
     let tempCart = [...this.state.cart];

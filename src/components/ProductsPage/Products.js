ff857e5334639516ac5908c782a06be4de77db5f
Phonekham <phone.madridista@gmail.com>
2019-08-25 16:32:06 +0700

    Filter Products Product Component Setup

--- src/components/ProductsPage/Products.js
+++ src/components/ProductsPage/Products.js
@@ -2,6 +2,7 @@ import React from "react";
 import { ProductConsumer } from "../../context/context";
 import Title from "../Title";
 import Product from "../Product";
+import ProductFilter from "./ProductFilter";
 
 export default function Products() {
   return (
@@ -13,11 +14,25 @@ export default function Products() {
             <div className="container">
               {/* title */}
               <Title center title="our products" />
+              <ProductFilter />
               {/* products */}
+              <div className="row">
+                <div className="col-10 mx-auto">
+                  <h6 className="text-title">
+                    Total Products : {filteredProducts.length}
+                  </h6>
+                </div>
+              </div>
               <div className="row py-5">
-                {filteredProducts.map(product => {
-                  return <Product key={product.id} product={product} />;
-                })}
+                {filteredProducts.length === 0 ? (
+                  <div className="col text-title text-center">
+                    sorry, no items matched your search
+                  </div>
+                ) : (
+                  filteredProducts.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  })
+                )}
               </div>
             </div>
           </section>
